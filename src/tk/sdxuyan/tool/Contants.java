d5d626d9d33b2c792b7fd3580105b9f3919ae29f
sdxy0506 <282353411@qq.com.address>
2013-09-26 22:28:29 +0800

    增加Contants类，用来存放静态数据

---
+++ src/tk/sdxuyan/tool/Contants.java
@@ -0,0 +1,7 @@
+package tk.sdxuyan.tool;
+
+public class Contants {
+	public static final int game_tip_number = 3;// 自动提示次数
+	public static final int game_refresh_number = 3;// 刷新次数
+	public static final int game_time_initial = 100;// 游戏初始时间
+}
