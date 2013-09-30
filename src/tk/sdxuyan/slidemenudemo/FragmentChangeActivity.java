7de01dd5d148024fdae81651057d9346a8868d4f
sdxy0506 <282353411@qq.com.address>
2013-09-30 21:21:40 +0800

    增加点击后退键弹出对话框的事件

--- src/tk/sdxuyan/slidemenudemo/FragmentChangeActivity.java
+++ src/tk/sdxuyan/slidemenudemo/FragmentChangeActivity.java
@@ -3,6 +3,8 @@ package tk.sdxuyan.slidemenudemo;
 import tk.sdxuyan.fragment.WelcomeFragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
+import android.widget.Toast;
 
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 import com.xuyan.happylink.R;
@@ -10,6 +12,7 @@ import com.xuyan.happylink.R;
 public class FragmentChangeActivity extends BaseActivity {
 
 	private Fragment mContent;
+	private long exitTime = 0;
 
 	public FragmentChangeActivity() {
 		super(R.string.app_name);
@@ -35,7 +38,7 @@ public class FragmentChangeActivity extends BaseActivity {
 		getSupportFragmentManager().beginTransaction()
 				.replace(R.id.menu_frame, new ColorMenuFragment()).commit();
 
-		//控制SlidingMenu的滑动打开方式
+		// 控制SlidingMenu的滑动打开方式
 		getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
 	}
 
@@ -50,6 +53,51 @@ public class FragmentChangeActivity extends BaseActivity {
 		getSupportFragmentManager().beginTransaction()
 				.replace(R.id.content_frame, fragment).commit();
 		getSlidingMenu().showContent();
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			// Dialog dialog = new AlertDialog.Builder(mContent.getActivity())
+			// .setIcon(R.drawable.buttons_bg20)
+			// .setTitle(R.string.quit)
+			// .setMessage(R.string.sure_quit)
+			// .setCancelable(false)
+			// .setPositiveButton(R.string.alert_dialog_ok,
+			// new DialogInterface.OnClickListener() {
+			// public void onClick(DialogInterface dialog,
+			// int whichButton) {
+			// mContent.getActivity().finish();
+			// }
+			// })
+			// .setNegativeButton(R.string.alert_dialog_cancel,
+			// new DialogInterface.OnClickListener() {
+			// public void onClick(DialogInterface dialog,
+			// int whichButton) {
+			//
+			// }
+			// }).create();
+			// dialog.show();
+			exit();
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
+	private void exit() {
+		if ((System.currentTimeMillis() - exitTime) > 2000) {
+			Toast.makeText(getApplicationContext(), "再按一次退出程序",
+					Toast.LENGTH_SHORT).show();
+			exitTime = System.currentTimeMillis();
+		} else {
+			finish();
+			System.exit(0);
+		}
+	}
+
+	@Override
+	protected void onDestroy() {
+		System.exit(0);
+		super.onDestroy();
 	}
 
 }
